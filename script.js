49b9ed8f652107ce23e0be2ea9e6e38f2d41f4da
Olivér Mészáros <procod@hotmail.com>
2022-02-17 21:15:04 +0100

    finished

--- script.js
+++ script.js
@@ -47,6 +47,50 @@ function playRound(playerSelection, computerSelection) {
     return result;
 }
 
+
+// containers and divs to show current game and running score
+const container = document.querySelector("body");
+const content = document.createElement("div");              //Result of 1 game
+const contentScore = document.createElement("div");         //Running score
+const winner = document.createElement("div");               //Winner of five games
+const notes = document.createElement("div");                //Starting new Game
+container.appendChild(content);
+container.appendChild(contentScore);
+container.appendChild(winner);
+container.appendChild(notes);
+let pScore = 0;
+let cScore = 0;
+
+content.style.cssText = "margin-top: 50px"                  //pushing the text down
+winner.style.cssText = "margin-top: 10px; font-weight: 900"
+notes.style.cssText = "margin-top: 25px"
+
+
 //buttons play games
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        winner.textContent = "";
+        notes.textContent = "";
+        content.textContent = playRound(button.textContent.toLowerCase(), computerPlay());
+        if (content.textContent == "You Win!") {
+            pScore = pScore + 1; // pScore += 1;
+        } else if (content.textContent == "You Lose!") {
+            cScore = cScore + 1; // cScore += 1;
+        }
+        contentScore.textContent = "You: " + pScore + " vs. Computer: " + cScore;
+        if (pScore == 5) {
+            winner.textContent = "The Winner is: You";
+            pScore = 0;
+            cScore = 0;
+            notes.textContent = "Starting a New Game!";
+        } else if (cScore == 5) {
+            winner.textContent = "The Winner is: Computer";
+            pScore = 0;
+            cScore = 0;
+            notes.textContent = "Starting a New Game!";
+        }
+    });
+});
 
 
